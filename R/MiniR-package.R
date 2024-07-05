a498b46863724415f3975e3edd4be4427a3fe505
FilippoIspanico <filippo.ispanico@gmail.com>
2024-07-05 12:19:27 +0200

    added doc

---
+++ R/MiniR-package.R
@@ -0,0 +1,10 @@
+#' MiniR: A Brief Description of Your Package
+#'
+#' A more detailed description of what your package does.
+#'
+#' @docType _PACKAGE
+#' @name MiniR
+NULL
+
+# Use _PACKAGE tag to mark this file as package documentation
+"_PACKAGE"
