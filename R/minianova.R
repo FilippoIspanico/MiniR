366ef6642991ae881a1a401ee3820b138607d826
FilippoIspanico <filippo.ispanico@gmail.com>
2024-07-06 18:20:36 +0200

    added manova bonferroni function

--- R/minianova.R
+++ R/minianova.R
@@ -287,3 +287,296 @@ colMeans = function(data){
 
 
 
+tau = function(data, treat, effect, treat2 = NULL, effect2 = NULL){
+  
+  if(is.null(treat2) | is.null(effect2))
+    return(MiniR::colMeans(data[which(effect == treat) ,]))
+  return(MiniR::colMeans(data[which(effect == treat & effect2 == treat2) ,]))
+  
+}
+
+
+
+CI.diff = function(tau1, tau2, pos, n1, n2, k.bonf, alpha, fit.manova){
+  
+  Spool = summary(fit.manova)$SS$Residuals
+  Spool = Spool/fit.manova$df.residual
+  
+  quantile = qt(1-alpha/2/k, fit.manova$df.residual)
+  correction = sqrt(Spool[pos, pos]*(1/n1 + 1/n2))
+  
+  ci = cbind(
+    inf = tau1[pos] - tau2[pos] - quantile*correction,
+    sup = tau1[pos] - tau2[pos] + quantile*correction
+  )
+  
+  return(ci)
+  
+}
+# n1 & n2 are not the observations per group! 
+# they are the number of observations that are used to compute tau1 & tau2!
+
+
+
+CI.effect = function(tau1, pos, n1, x.mean, n.tot, k.bonf, alpha, fit.manova){
+  
+  Spool = summary(fit.manova)$SS$Residuals
+  Spool = Spool/fit.manova$df.residual
+  
+  quantile = qt(1-alpha/2/k, fit.manova$df.residual)
+  correction = sqrt(Spool[pos, pos]*(1/n1 + 1/n.tot))
+  
+  ci = cbind(
+    inf = tau1[pos] - x.mean[pos] - quantile*correction,
+    sup = tau1[pos] - x.mean[pos] + quantile*correction
+  )
+  
+  return(ci)
+  
+}
+
+
+
+CI = function(tau1, pos, n1, k.bonf, alpha, fit.manova){
+  
+  Spool = summary(fit.manova)$SS$Residuals
+  Spool = Spool/fit.manova$df.residual
+  
+  quantile = qt(1-alpha/2/k, fit.manova$df.residual)
+  correction = sqrt(Spool[pos, pos]*(1/n1))
+  
+  ci = cbind(
+    inf = tau1[pos] - quantile*correction,
+    sup = tau1[pos] + quantile*correction
+  )
+  
+  return(ci)
+  
+  
+  
+}
+
+
+
+
+#' Compute Bonferroni Intervals for MANOVA or ANOVA
+#'
+#' This function calculates Bonferroni intervals for Multivariate Analysis of Variance (MANOVA) 
+#' or univariate Analysis of Variance (ANOVA). It can handle one-way or two-way designs.
+#' The function provides confidence intervals for differences between treatment effects 
+#' and for the effects themselves.
+#'
+#' @param fit.manova An object representing a fitted MANOVA or ANOVA model.
+#' @param data A dataframe containing the response variable(s).
+#' @param effects1 A factor representing the first treatment effect.
+#' @param effect1.name A character string for the name of the first effect (currently unused in the function).
+#' @param effects2 An optional factor representing the second treatment effect for two-way designs. Default is NULL.
+#' @param effects2.name A character string for the name of the second effect (currently unused in the function).
+#' @param k.bonf The Bonferroni correction factor. Must be provided.
+#' @param alpha The significance level for the confidence intervals.
+#'
+#' @return A list containing the following elements:
+#'   \item{tau1}{A dataframe of estimated effects for the first treatment.}
+#'   \item{tau2}{A dataframe of estimated effects for the second treatment (if applicable).}
+#'   \item{general.mean}{A vector of overall means for each response variable.}
+#'   \item{p}{The number of response variables (1 for ANOVA, >1 for MANOVA).}
+#'   \item{ci.diff.tau1}{A dataframe of confidence intervals for differences between effects of the first treatment.}
+#'   \item{ci.diff.tau2}{A dataframe of confidence intervals for differences between effects of the second treatment (if applicable).}
+#'   \item{ci.effect.tau1}{A dataframe of confidence intervals for effects of the first treatment.}
+#'   \item{ci.effect.tau2}{A dataframe of confidence intervals for effects of the second treatment (if applicable).}
+#'
+#' @details
+#' This function can be used for both MANOVA and ANOVA:
+#' - For ANOVA, the input data should have a single response variable (p = 1).
+#' - For MANOVA, the input data should have multiple response variables (p > 1).
+#' 
+#' The function automatically adapts to the number of response variables in the input data.
+#' 
+#' For one-way designs, use only the `effects1` parameter. For two-way designs, 
+#' use both `effects1` and `effects2`.
+#'
+#' @export
+#' @export manova.bonferroni
+#'
+#' @examples
+#' \dontrun{
+#' # For one-way ANOVA
+#' anova_results <- manova.bonferroni(fit.anova, my_data, effects1 = factor(treatment),
+#'                                    k.bonf = 3, alpha = 0.05)
+#' 
+#' # For one-way MANOVA
+#' manova_results <- manova.bonferroni(fit.manova, my_data, effects1 = factor(treatment),
+#'                                     k.bonf = 4, alpha = 0.05)
+#' 
+#' # For two-way ANOVA
+#' two_way_anova_results <- manova.bonferroni(fit.anova, my_data, 
+#'                                            effects1 = factor(treatment1),
+#'                                            effects2 = factor(treatment2),
+#'                                            k.bonf = 6, alpha = 0.05)
+#' 
+#' # For two-way MANOVA
+#' two_way_manova_results <- manova.bonferroni(fit.manova, my_data, 
+#'                                             effects1 = factor(treatment1),
+#'                                             effects2 = factor(treatment2),
+#'                                             k.bonf = 8, alpha = 0.05)
+#'                                             }
+#' 
+manova.bonferroni = function(fit.manova, data, effects1, effects2 = NULL, k.bonf, alpha){
+  
+  if (!is.data.frame(data)) {
+    stop("'data' must be a dataframe")
+  }
+  
+  if (!is.factor(effects1)) {
+    stop("'effects1' must be a factor")
+  }
+  
+  if (!is.null(effects2) && !is.factor(effects2)) {
+    stop("'effects2' must be a factor or NULL")
+  }
+  
+  p = ncol(data)
+  
+  treatments1 = levels(effects1)
+  treatments2 = levels(effects2)
+  
+  tau1 = matrix(0, nrow = p, ncol = length(treatments1))
+  tau1 = data.frame(tau1)
+  colnames(tau1) = treatments1
+  
+  tau2 =  matrix(0, nrow = p, ncol = length(treatments2))
+  tau2 = data.frame(tau2)
+  colnames(tau2) = treatments2
+  
+  general.mean = MiniR::colMeans(data)
+  
+  
+  rownames = NULL
+  
+  for(treat in treatments1){
+    rownames = names(tau(data, treat, effects1))
+    
+    tau1[, treat] = tau(data, treat, effects1)
+  }
+  
+  
+  row.names(tau1) = rownames
+  rownames = NULL
+  # tau1 now contains the effect tau of effect1 
+  for(treat in treatments2){
+    rownames = names(tau(data, treat, effects2))
+    
+    tau2[, treat] = tau(data, treat, effects2)
+  }
+  
+  row.names(tau2) = rownames
+  
+  # now we compute the CI for the differences of effects1
+  g = length(treatments1)
+  ci.diff = matrix(0, nrow = p*g*(g-1)/2, ncol = 2)
+  ci.diff = data.frame(ci.diff)
+  colnames(ci.diff) = c("inf", "sup")
+  row = 1
+  rownames = character(p*g*(g-1)/2)
+  for(i in 1:(length(treatments1)-1)){
+    for(j in (i+1):length(treatments1))
+      for(pos in 1:p){
+        # here we compute the CI for the differences between tau1[i], tau1[j] in pos k
+        n1 = length(which(effects1 == treatments1[i]))
+        n2 = length(which(effects1 == treatments1[j]))
+        
+        ci.diff[row, ] = CI.diff(tau1[, i], tau1[, j], pos, n1, n2, k.bonf, alpha, fit.manova = fit.manova)
+        rownames[row] = sprintf("%s - %s [%d]", treatments1[i], treatments1[j], pos)
+        row = row + 1
+      }
+  }
+  rownames(ci.diff) = rownames
+  ci.diff.tau1 = ci.diff
+  
+  
+  
+  
+  ci.diff.tau2 = NULL
+  if(!is.null(effects2)){
+    g = length(treatments2)
+    ci.diff = matrix(0, nrow = p*g*(g-1)/2, ncol = 2)
+    ci.diff = data.frame(ci.diff)
+    colnames(ci.diff) = c("inf", "sup")
+    row = 1
+    rownames = character(p*g*(g-1)/2)
+    for(i in 1:(length(treatments2)-1)){
+      for(j in (i+1):length(treatments2))
+        for(pos in 1:p){
+          # here we compute the CI for the differences between tau1[i], tau1[j] in pos k
+          n1 = length(which(effects2 == treatments2[i]))
+          n2 = length(which(effects2 == treatments2[j]))
+          
+          ci.diff[row, ] = CI.diff(tau2[, i], tau2[, j], pos, n1, n2, k.bonf, alpha, fit.manova = fit.manova)
+          rownames[row] = sprintf("%s - %s [%d]", treatments2[i], treatments2[j], pos)
+          row = row + 1
+        }
+    }
+    rownames(ci.diff) = rownames
+    ci.diff.tau2 = ci.diff
+  }
+  
+  
+  
+  
+  
+  
+  g = length(treatments1)
+  ci.effect.tau1 = matrix(0, nrow = g*p, ncol = 2)
+  ci.effect.tau1 = data.frame(ci.effect.tau1)
+  colnames(ci.effect.tau1) = c("inf", "sup")
+  row = 1
+  rownames = character(p*g)
+  for(i in 1:length(treatments1)){
+    for(pos in 1:p){
+      n1 = length(which(effects1 == treatments1[i]))
+      ci.effect.tau1[row, ] = CI.effect(tau1[,i], pos = pos, n1 = n1, x.mean = general.mean, n.tot = nrow(data),
+                                        k.bonf = k.bonf, alpha = alpha, fit.manova = fit.manova)
+      rownames[row] = sprintf("%s [%d]", treatments1[i], pos)
+      row = row + 1                             
+      
+    }
+    
+  }
+  rownames(ci.effect.tau1) = rownames
+  
+  ci.effect.tau2 = NULL
+  
+  if(!is.null(effects2)){
+    g = length(treatments2)
+    ci.effect.tau2 = matrix(0, nrow = g*p, ncol = 2)
+    ci.effect.tau2 = data.frame(ci.effect.tau2)
+    colnames(ci.effect.tau2) = c("inf", "sup")
+    row = 1
+    rownames = character(p*g)
+    for(i in 1:length(treatments2)){
+      for(pos in 1:p){
+        n1 = length(which(effects2 == treatments2[i]))
+        ci.effect.tau2[row, ] = CI.effect(tau2[,i], pos = pos, n1 = n1, x.mean = general.mean, n.tot = nrow(data),
+                                          k.bonf = k.bonf, alpha = alpha, fit.manova = fit.manova)
+        rownames[row] = sprintf("%s [%d]", treatments2[i], pos)
+        row = row + 1                             
+        
+      }
+      
+    }
+    rownames(ci.effect.tau2) = rownames
+  }
+  
+  
+  
+  
+  
+  result = list(tau1 = tau1, tau2 = tau2, general.mean = general.mean, p = p,
+                ci.diff.tau1 = ci.diff.tau1, ci.diff.tau2 = ci.diff.tau2,
+                ci.effect.tau1 = ci.effect.tau1, ci.effect.tau2 = ci.effect.tau2)
+  return(result)
+}
+
+
+
+
